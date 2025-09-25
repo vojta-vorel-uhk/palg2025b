0277d22d5983b039ab1aaa6c98109d19ac67626a
Vojta Vorel <vorel.vojtech@gmail.com>
2025-09-25 14:59:57 +0200

    Úloha 5

---
+++ src/main/java/palg/Cviko01_Uloha05.java
@@ -0,0 +1,22 @@
+package palg;
+
+import java.util.Scanner;
+
+public class Cviko01_Uloha05 {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Zadej město:");
+            Scanner s = new Scanner(System.in);
+            String city = s.nextLine();
+            if (isOk(city))
+                System.out.println("OK");
+            else
+                System.out.println("NOT_OK");
+        }
+    }
+
+    public static boolean isOk(String input)
+    {
+       return input.chars().distinct().count() >= input.length();
+    }
+}
