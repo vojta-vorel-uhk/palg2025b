4bb86c848587595e6a202b3886149488c4287a93
Vojta Vorel <vorel.vojtech@gmail.com>
2025-10-02 10:43:52 +0200

    Příprava na cviko 2

---
+++ src/main/java/palg/Cviko02_Uloha00.java
@@ -0,0 +1,26 @@
+package palg;
+
+public class Cviko02_Uloha00 {
+    public static void main(String[] args)
+    {
+        int a= 0;
+        int b= 0;
+        double c = 0;
+        double d = 0;
+        int e1 = 0;
+        int e2=0;
+        //System.out.println(5 OP 1 == 10); // Doplň operátor aby se vypsalo true
+        //System.out.println(5 OP 10 == 5); // Doplň operátor aby se vypsalo true
+        //System.out.println(5 OP 6 == 0); // Doplň operátor aby se vypsalo true
+        //System.out.println(-7 OP 5 == -2); // Doplň operátor aby se vypsalo true
+        //System.out.println(9 OP 1 == 4); // Doplň operátor aby se vypsalo true
+        System.out.println(a > a + 1); // Doplň hodnotu a aby se vypsalo true
+        System.out.println(b < 0 && Math.abs(b) == b); // Doplň hodnotu b aby se vypsalo true
+        System.out.println( 3 * c / 10 != 0.3 * c); // Doplň hodnotu c aby se vypsalo true
+        System.out.println( !(d >= 0) && !(d <= 0) ); // Doplň hodnotu d aby se vypsalo true
+        System.out.println((8 & e1) == 8); // Doplň hodnotu e1 aby se vypsalo true
+        System.out.println((6 | e2) == 14); // Doplň hodnotu e2 aby se vypsalo true
+
+
+    }
+}
