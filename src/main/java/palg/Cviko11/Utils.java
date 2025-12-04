b9585d200aad783099cdeaa0565e77de3e807b72
Vojta Vorel <vorelvo2@uhk.cz>
2025-12-04 14:37:10 +0100

    Po cviku 10

--- src/main/java/palg/Cviko11/Utils.java
+++ src/main/java/palg/Cviko11/Utils.java
@@ -10,6 +10,23 @@ public class Utils {
             String[] target,
             int targetStart)
     {
-
+        int next1 = 0;
+        int next2 = 0;
+        for(int t=0;t<source1.length+source2.length;t++)
+        {
+            if(next1 >= source1.length){
+                target[targetStart + t] = source2[next2];
+                next2++;
+            } else if(next2 >= source2.length){
+                target[targetStart + t] = source1[next1];
+                next1++;
+            } else if(  source1[next1].compareTo(source2[next2]) < 0 ){
+                target[targetStart + t] = source1[next1];
+                next1++;
+            } else {
+                target[targetStart + t] = source2[next2];
+                next2++;
+            }
+        }
     }
 }
