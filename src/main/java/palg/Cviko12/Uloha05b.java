286bb980f204f3e1e7150c161fae0fca1b501fa7
Vojta Vorel <vorel.vojtech@gmail.com>
2025-12-18 11:14:41 +0100

    Příprava na cviko 13

---
+++ src/main/java/palg/Cviko12/Uloha05b.java
@@ -0,0 +1,17 @@
+package palg.Cviko12;
+
+import java.time.*;
+
+public class Uloha05b
+{
+    public static void main(String[] args)
+    {
+        ZoneId newYorkZone = ZoneId.of("America/New_York");
+        var wallclock = LocalDateTime.of(2025,12,18,19,20,0);
+        var zoned = ZonedDateTime.of(wallclock,newYorkZone);
+        var instant = zoned.toInstant();
+
+        // TODO: Hodiny na zdi v New Yorku ukazovaly 18.12.2025, 19:20
+        //  Vypiš, jaký datum a čas byl v Praze
+    }
+}
---
+++ src/main/java/palg/Cviko12/Uloha07b.java
@@ -0,0 +1,34 @@
+package palg.Cviko12;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Uloha07b
+{
+    public static void main(String[] args)
+    {
+        // TODO 1: Oprav regulární výraz, aby se vypisovala data
+        String string = "Testy budou dva, jeden na sedmém cvičení (6.11.2025), druhý na posledním cvičení (18.12.2025). ";
+        System.out.println("Datumy:");
+        printAllMatches(string,Pattern.compile("\\d+"));
+
+        // TODO 2: Oprav regulární výraz, aby se vypisovala města
+        String string2="Nové Město nad Metují + Náchod + Trutnov / Kostelec nad Orlicí / Brandýs nad Labem-Stará Boleslav";
+        System.out.println("Města:");
+        printAllMatches(string2,Pattern.compile("u."));
+
+        // TIP: https://regex101.com/
+
+    }
+
+    public static void printAllMatches(String string, Pattern regex){
+        for(var m :allMatches(string,regex) ) {
+            System.out.println(m);
+        }
+    }
+    
+    public static List<String> allMatches(String string, Pattern regex){
+        var outputStream = regex.matcher(string).results().map(m->m.group()) ;
+        return outputStream.toList();
+    }
+}
